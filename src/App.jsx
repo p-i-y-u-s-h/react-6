3a5cc0f8afecfc38bde3defe135e02518d401afb
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-12 23:43:55 +0530

    commit-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import { useEffect } from "react"
+import { useState } from "react"
 
 function App(){
+  const [currentTab,setCurrentTab] = useState("feed")
+
+  useEffect(function(){
+    
+  },[currentTab])
+
   return <div>
-    <button>Feed</button>
-    <button>Notification</button>
-    <button>Messages</button>
-    <button>Jobs</button>
+    <button onClick={()=>{
+      setCurrentTab("feed")
+    }} style={{color : currentTab == "feed" ? "red" : "black"}}>Feed</button>
+    <button onClick={()=>{
+      setCurrentTab("Notification")
+    }} style={{color : currentTab == "Notification" ? "red" : "black"}}>Notification</button>
+    <button onClick={()=>{
+      setCurrentTab("Messages")
+    }} style={{color : currentTab == "Messages" ? "red" : "black"}}>Messages</button>
+    <button onClick={()=>{
+      setCurrentTab("Jobs")
+    }} style={{color : currentTab == "Jobs" ? "red" : "black"}}>Jobs</button>
   </div>
 
 }
