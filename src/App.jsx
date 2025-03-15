aee3da03fb7a897ebfe025938d834622204659c1
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-15 23:41:31 +0530

    commit-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useState } from "react"
 
 function App(){
+  
   const [currentTab,setCurrentTab] = useState(1);
   const [TabData,setTabData] = useState({});
   const [loading,setLoading] = useState(true);
